f231bc56b48d7fdd3aecb5a0820bcbe17784cecb
Osiris <saadcarnix012@gmail.com>
2022-06-08 17:50:52 +0500

    new things added

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require("express");
-var bodyParser = require("body-parse");
 var app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// var bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.render("home");
+  res.render("home", { task: tasks, complete: complete });
+});
+
+var tasks = ["Learning Arabic", "Reading Qur'an", "Learning Node.js"];
+
+app.post("/addtask", function (req, res) {
+  var newTask = req.body.newtask;
+  tasks.push(newTask);
+  res.redirect("/");
 });
 
 app.listen(3100, function () {
