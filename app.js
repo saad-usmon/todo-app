eb12a4913cc7eab827d6a4968014a07c833be60e
Osiris <saadcarnix012@gmail.com>
2022-06-08 17:00:51 +0500

    last change

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require("express");
+var bodyParser = require("body-parse");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Hello, World!");
+  res.render("home");
 });
 
 app.listen(3100, function () {
